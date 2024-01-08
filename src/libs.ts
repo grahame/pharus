6a12c4443e28045a04b35e6aee77c5c1778f54f6
Grahame Bowland <grahame@bowland.au>
2024-01-08 23:51:32 +0800

    bump church-calendar

--- src/libs.ts
+++ src/libs.ts
@@ -3,13 +3,13 @@ export { Client as MQTTClient } from "https://deno.land/x/mqtt@0.1.2/deno/mod.ts
 export {
     aca_seasons,
     calendar as aca_calendar,
-} from "https://deno.land/x/churchcalendar@1.4.4/western/calendars/anglican-church-of-australia.ts";
-export { LiturgicalColour } from "https://deno.land/x/churchcalendar@1.4.4/calendar.ts";
+} from "https://deno.land/x/churchcalendar@1.4.5/western/calendars/anglican-church-of-australia.ts";
+export { LiturgicalColour } from "https://deno.land/x/churchcalendar@1.4.5/calendar.ts";
 export type {
     LiturgicalSeason,
     ResolvedCalendar,
     ResolvedObservance,
-} from "https://deno.land/x/churchcalendar@1.4.4/calendar.ts";
-export { getLiturgicalYear } from "https://deno.land/x/churchcalendar@1.4.4/western/liturgicalyear.ts";
-export { get_season_for_date } from "https://deno.land/x/churchcalendar@1.4.4/western/seasons/index.ts";
+} from "https://deno.land/x/churchcalendar@1.4.5/calendar.ts";
+export { getLiturgicalYear } from "https://deno.land/x/churchcalendar@1.4.5/western/liturgicalyear.ts";
+export { get_season_for_date } from "https://deno.land/x/churchcalendar@1.4.5/western/seasons/index.ts";
 export { getSunrise } from "npm:sunrise-sunset-js";
